f33a912fe70bd173ad098f813cb436d6642a2e8a
Helina Alex <helinatalex@gmail.com>
2026-03-28 09:56:20 +0530

    erroe corrected

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -4,7 +4,7 @@ public class PatternPrinting {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("Enter n: ");
+      
         int n = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
