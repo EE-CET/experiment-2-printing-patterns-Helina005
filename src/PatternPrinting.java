f9265951ddd92defe7baf4990979a1d6a8524d9c
Helina Alex <helinatalex@gmail.com>
2026-03-28 09:47:24 +0530

    added

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -8,7 +8,7 @@ public class PatternPrinting {
         int n = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
-            System.out.print("*\t");
+            System.out.print("* ");
         }
         System.out.println();
 
